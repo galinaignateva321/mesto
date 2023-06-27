da01745d1efdc3147c12934a32addfae5be6427b
Galina Ignateva <galinaignateva321@yandex.ru>
2023-06-27 14:46:12 +0300

    startSprint5

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
 const editButtonElement = document.querySelector('.profile__edit-button');
-const popupElement = document.querySelector('.popup');
-const closeButtonElement = document.querySelector('.popup__close-button');
+const popupElement = document.querySelector('.popup-edit-profile');
+const closeButtonElement = document.querySelector('.popup-edit-profile__close-button');
 
-const nameInputElement = document.querySelector('.popup__input_el_name');
-const jobInputElement = document.querySelector('.popup__input_el_job');
+const nameInputElement = document.querySelector('.popup-edit-profile__input_el_name');
+const jobInputElement = document.querySelector('.popup-edit-profile__input_el_job');
 const nameEditElement = document.querySelector('.profile__title');
 const jobEditElement = document.querySelector('.profile__subtitle');
-const formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup-edit-profile__form');
 
 function handlEditClick() {
-  popupElement.classList.add('popup_opened')
+  popupElement.classList.add('popup-edit-profile_opened')
   nameInputElement.value = nameEditElement.textContent
   jobInputElement.value = jobEditElement.textContent
 }
 editButtonElement.addEventListener('click', handlEditClick);
 
-function handleClosePopup() {
-  popupElement.classList.remove('popup_opened')
+function handleClosePopupEditProfile() {
+  popupElement.classList.remove('popup-edit-profile_opened')
 }
-closeButtonElement.addEventListener('click', handleClosePopup);
+closeButtonElement.addEventListener('click', handleClosePopupEditProfile);
 
 function handleFormSubmit(evt) {
   evt.preventDefault()
   nameEditElement.textContent = nameInputElement.value
   jobEditElement.textContent = jobInputElement.value
-  handleClosePopup()
+  handleClosePopupEditProfile()
 }
 formElement.addEventListener('submit', handleFormSubmit);
-
